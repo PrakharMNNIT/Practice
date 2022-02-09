8a8d196e0485fc007a51f606dc0579d0a8767712
Prakhar Parthasarthi <prakhapa@amazon.com>
2022-02-09 20:37:03 +0530

    Initial commit

---
+++ src/com/company/ass1/Rectangle.java
@@ -0,0 +1,9 @@
+package com.company.ass1;
+//
+//public class Rectangle {
+//}
+//    Create a new Rectangle.java file.
+//    A Rectangle should be represented by the field variables length and breadth each of data type double.
+//    Define a parameterized constructor Rectangle.
+//    Define getters public String getLength() and public String getBreadth().
+//    Define a method public String toString() that returns a string in the following example format:
